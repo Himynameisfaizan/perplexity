fb393dcfab68003741b4801b87f00e0c4a68ea45
himynameisfaizan <faizansaikh830@gmail.com>
2026-03-24 14:53:45 +0530

    interated ai model

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./src/app.js";
+import readline from 'readline';
 import database from "./src/config/database.js";
-
 database();
 
 const port = process.env.PORT || 3000;
@@ -9,3 +9,12 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`server is running in port ${port}`);
 });
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What is your name? ", (name)=>{
+  console.log(`Hello ${name}`);
+})
--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
+import { testAi } from "./services/ai.service.js";
+testAi()
 
 const app = express();
 app.use(express.json());
